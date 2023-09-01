5179607b6859c950a8ea7f76e267d470fe9218fd
isabellins <125700381+isabellins@users.noreply.github.com>
2023-09-01 18:21:33 -0300

    Add files via upload

---
+++ projeto-87/main.js
@@ -0,0 +1,58 @@
+
+var canvas = fabric.Canvas('myCanvas');
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage) {
+    fabric.Image.fromURL(getImage, function(Img){
+        blocoObj = Img;
+        blocoObj.scaleToWidth(widthBlocoImg);
+        blocoObj.scaleToHeight(heightBlocoImg);
+        blocoObj.set({
+            top: playerY, left: playerX
+        })
+        canvas.add(blocoObj);
+    })
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		new_image('gr.png')
+		console.log("g")
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		new_image('yr.png')
+		console.log("y")
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('pr.png')
+		console.log("p")
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		new_image('br.png')
+		console.log("b")
+	}
+	
+}
+
